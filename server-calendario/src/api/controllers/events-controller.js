52c883a856d7b46f484e6e3f910799bce7fd9fb7
Douglas Alves <douglas.expalves@gmail.com>
2018-05-23 15:28:39 -0300

    controladores de usuario e evento

---
+++ server-calendario/src/api/controllers/events-controller.js
@@ -0,0 +1,68 @@
+import { Router } from 'express'
+
+import { sessionCheckers, eventCheckers } from './../../helpers/checkers'
+import { createEvent, deleteEvent, updateEvent, findByAuthor } from '../services/events-services'
+
+const routing = Router()
+
+routing.post('/events', sessionCheckers, eventCheckers, async (req, res) => {
+  const { author, name, initialDate, finalDate, initialTime, finalTime } = req.body
+  try {
+    const data = await createEvent({ name, initialDate, finalDate, initialTime, finalTime, author })
+    const status = 201
+
+    return res.status(status).send({ data, status })
+  } catch (err) {
+    return res.status(err.status).send({ data: { message: err.message, code: err.code}, status: err.status })
+  }
+})
+
+
+routing.get('/events/author/:author', sessionCheckers, async (req, res) => {
+  const { author } = req.params
+
+  try {
+    const data = await findByAuthor({ author })
+    
+    return res.send({ data, status: 201 }).status(201)
+  } catch (err) {
+    return res.status(err.status).send({ data: { message: err.message, code: data.code }, status: data.status })
+  }
+})
+
+routing.put('/events/:_id/author/:author', sessionCheckers, eventCheckers, async (req, res) => {
+  const { _id, author } = req.params
+  const { name, initialDate, finalDate, initialTime, finalTime } = req.body
+
+  try {
+    const data = await updateEvent({ _id, name, initialDate, finalDate, initialTime, finalTime, author })
+    return res.send({
+      data,
+      status: 201
+    }).status(201)
+  } catch (err) {
+    res.status(err.status)
+    .send({ 
+      data: {
+        message: err.message,
+        code: err.code,
+      },
+      status: err.status
+    })
+  }
+ 
+})
+
+routing.delete('/events/:_id/author/:author', sessionCheckers,  async (req, res) => {
+  const { _id, author } = req.params
+  
+  try {
+    const data = await deleteEvent({ _id, author })
+    
+    return res.send({ data, status: 200 }).status(200)
+  } catch (err) {
+    return res.status(err.status).send({ data: { message: err.message, code: err.code }, status: err.status })    
+  }
+})
+
+export default routing
