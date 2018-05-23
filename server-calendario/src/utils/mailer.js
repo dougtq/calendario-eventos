4e4e4f48b039ffcd886e071f2bec7c7f74216671
Douglas Alves <douglas.expalves@gmail.com>
2018-05-23 15:28:56 -0300

    prepara e-mail

---
+++ server-calendario/src/utils/mailer.js
@@ -0,0 +1,20 @@
+import { createTransport } from 'nodemailer'
+import hbs from 'nodemailer-express-handlebars'
+import { resolve } from 'path'
+
+const transport = createTransport({
+	host: process.env.MAILER_HOST,
+	port: process.env.MAILER_PORT,
+	auth: {
+		user: process.env.MAILER_USER,
+		pass: process.env.MAILER_PASS
+	}
+})
+
+transport.use('compile', hbs({
+	viewEngine: 'handlebars',
+	viewPath: resolve('./src/resources/mail'),
+	extName: '.html'
+}))
+
+export default transport
